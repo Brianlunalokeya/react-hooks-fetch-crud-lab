ce2dac0f281063c272a411fcd3477169f171fd31
+ <+>
2023-04-30 15:29:16 +0300

    Done with Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((questions) => setQuestions(questions));
+  }, []);
+
+  function handleAddQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion]);
+  }
+
+  const handleDeleteQuestion = (id) => {
+    fetch(`http://localhost:4000/questions/${id}`, { method: "DELETE" })
+      .then(() => {
+        setQuestions(questions.filter((question) => question.id !== id));
+      })
+      .catch((error) => console.error("Error deleting question:", error));
+  };
+
+  const handleUpdateQuestion = (id, updatedQuestion) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedQuestion),
+    })
+      .then(() => {
+        const updatedQuestions = questions.map((question) =>
+          question.id === id ? updatedQuestion : question
+        );
+        setQuestions(updatedQuestions);
+      })
+      .catch((error) => console.error("Error updating question:", error));
+  };
+
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={handleAddQuestion} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDeleteQuestion={handleDeleteQuestion}
+          onUpdateQuestion={handleUpdateQuestion}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,23 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, onDeleteQuestion, onUpdateQuestion }) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul className="Questions">
+        {questions.map((question) => (
+          <QuestionItem 
+            key={question.id} 
+            question={question} 
+            onDelete={onDeleteQuestion} 
+            onUpdate={onUpdateQuestion}
+            />
+        ))}
+      </ul>
     </section>
   );
 }
 
 export default QuestionList;
+
